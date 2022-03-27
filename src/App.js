2dd2c6d4b0d0099fd71b5fb1576c5f5626629d05
CaileyManktelow <cailey.manktelow@gmail.com>
2022-03-27 12:31:45 -0400

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ function App() {
   return (
     <div className="App">
       <Dictionary />
+      <span>
+        Coded by Cailey Manktelow. Open sourced on{" "}
+        <a
+          href="https://github.com/CaileyManktelow"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        .{" "}
+      </span>
     </div>
   );
 }
